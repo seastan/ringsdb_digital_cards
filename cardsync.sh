7d145161970b58bc7202eda2e6b52a80f7bcd5db
Alberto Wagner <alberto.wagner@gmai.com>
2019-02-19 19:17:00 -0300

    ringsdb cards

--- cardsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Rename plain-english image names in ./images to the coded image names for ringsdb in ./renamed 
-python3.5 rename.py
-
-# Syncronize ./renamed with ringsdb
-rsync -v ./renamed/* rings@ringsdb.com:/var/www/ringsdb_digital/web/bundles/cards/
-
